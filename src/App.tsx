378e486c80b165835764634493e6ad501956e651
Natalie Yip <yip.natalie92@gmail.com>
2025-06-06 05:22:16 -0700

    Remove client key

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { WidgetContainer } from './components/WidgetContainer';
 function App() {
     return (
         <>
-            <WidgetContainer clientKey="test" />
+            <WidgetContainer />
         </>
     );
 }
--- src/components/WidgetContainer.test.tsx
+++ src/components/WidgetContainer.test.tsx
@@ -30,7 +30,7 @@ vi.mock('../services/widget.service', () => ({
 
 describe('WidgetContainer', () => {
     it('renders widget data from API', async () => {
-        render(<WidgetContainer clientKey="" />);
+        render(<WidgetContainer />);
 
         const title = screen.getByTestId('header');
         const underwriter = screen.getByTestId('underwriter');
